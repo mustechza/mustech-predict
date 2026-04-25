8ba097ff86a37cae6ef1333807b84855fc969b5c
mustechza <141557155+mustechza@users.noreply.github.com>
2026-04-25 16:21:18 +0200

    Update loader.py

--- data/loader.py
+++ data/loader.py
@@ -2,13 +2,4 @@ import json
 
 def load_json(file):
     data = json.load(file)
-
-    history = []
-
-    for entry in data:
-        try:
-            history.append(float(entry["rate"]))
-        except:
-            continue
-
-    return history
+    return [float(x["rate"]) for x in data if "rate" in x]

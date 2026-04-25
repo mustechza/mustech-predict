cc04836de1d99b72e0e0a609a6146cfd244e164d
mustechza <141557155+mustechza@users.noreply.github.com>
2026-04-25 14:31:03 +0200

    Create lstm_model.py

---
+++ lstm_model.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+class LSTMModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.lstm = nn.LSTM(1, 32, batch_first=True)
+        self.fc = nn.Linear(32, 1)
+
+    def forward(self, x):
+        out, _ = self.lstm(x)
+        out = out[:, -1, :]
+        return self.fc(out)

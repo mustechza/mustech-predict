010e762a60b83aca2ec75fa3e05f335cbe400e24
mustechza <141557155+mustechza@users.noreply.github.com>
2026-04-25 15:07:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,14 @@
 
 import streamlit as st
-import os
-st.write(os.listdir())
-st.write(os.listdir("models"))
 from data.loader import load_json
 from models.lstm import LSTMModel
+import sys
+import os
+
+sys.path.append(os.path.dirname(__file__))
+
 from models.agent import Agent
-from training.pretrain import pretrain
+from models.lstm import LSTMModelfrom training.pretrain import pretrain
 from training.utils import build_state
 
 st.title("🚀 Hybrid Crash AI Pro")
